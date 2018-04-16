6da7741e48ce5a8ed4b3748ca09f81e16f5b7b4f
Sesh Sadasivam <seshrs@umich.edu>
2018-04-16 13:16:06 -0400

    Added most of the Infra for Refactored Search

--- ws/search_refactor/__definitions__/ICommandController.php
+++ ws/search_refactor/__definitions__/ICommandController.php
@@ -9,7 +9,7 @@
  * Implement this interface to create a new Command.
  * Don't forget to `make build` after adding a new command.
  */
-interface iCommandController {
+interface ICommandController {
   /**
    * Returns a list of command keywords that this Command should respond to.
    * @return array<string>: List of command keywords representing this Command.
@@ -22,7 +22,7 @@ interface iCommandController {
    * The query can be an empty string. The user will be redirected to the
    * URL returned by this function.
    * @param string: The user's query. Can be an empty string.
-   * @return URL: A URL object
+   * @return Result: A Result object
    */
   public function executeQuery(string $query);
 }
--- ws/search_refactor/__search__/SearchController.php
+++ ws/search_refactor/__search__/SearchController.php
@@ -6,60 +6,80 @@
  */
 
 class SearchController {
-  public static function search() {
+  /**
+   * This is the entry point for the search controller. It parses the request
+   * from GET/POST parameters (or from the custom query), sends the redirection
+   * header and logs the request.
+   * 
+   * @param {string} [$custom_request = null] - An optional custom delegate query
+   * @return {void}
+   */
+  public static function search($custom_request = null) {
     $command_map = self::fetchCommandMap();
-    $request = self::parseRequest($command_map);
+    $request = self::parseRequest($command_map, $request);
     $redirect_url = $request['command_executor']();
     self::redirectUser($redirect_url, $request);
     self::endConnection();
     self::logRequest($request, $request);
   }
 
+  /**
+   * Fetches the pre-built command-metadata map.
+   * 
+   * @return {dict<string, string>}
+   */
   private static function fetchCommandMap() {
     $command_map_json_filepath = __DIR__ . '/../__build__/commandMap.json';
     $command_map_JSON = file_get_contents($command_file_map_JSON);
     return json_decode($commandFileMapJSON);
   }
 
-  private static function parseRequest($command_map) {
+  private static function parseRequest($command_map, $custom_request = null) {
     require_once(__DIR__ . '/../../__util__/Sitevars.php');
 
-    // 1. Fetch and parse the query
-
-    // 1.1. Get the raw query
-    $post = isset($_GET['post']) ? $_GET['post'] : null;
-    if ($post && $post == '1') {
-      $raw_query = isset($_POST['q']) ? $_POST['q'] : '';
+    if ($custom_request && strlen($custom_request)) {
+      // 1.1 If a custom request has been provided, validate and use it.
+      $raw_query = $custom_request;
     }
     else {
-      $raw_query = isset($_GET['q']) ? $_GET['q'] : '';
+      // 1.2. Get the raw query from the HTTP Request
+      $post = isset($_GET['post']) ? $_GET['post'] : null;
+      if ($post && $post == '1') {
+        $raw_query = isset($_POST['q']) ? $_POST['q'] : '';
+      }
+      else {
+        $raw_query = isset($_GET['q']) ? $_GET['q'] : '';
+      }
     }
-    // 1.2. Parse the command and rest of the query
+
+    // 2. Parse the command and rest of the query.
     $query_terms = explode(' ', strtolower($query));
     $command = array_shift($query_terms);
     $query = implode(' ', $query_terms);
 
-    // 2. Retrieve the fallback command
+    // 3. Retrieve the fallback command
     $fallback_command = isset($_GET['fallback']) ? $_GET['fallback'] : null;
     if (!$fallback_command || !property_exists($command_map, $fallback_command)) {
       $fallback_command = Sitevars::FALLBACK_COMMAND;
     }
     $fallback_used = false;
 
-    // 3. Check whether the command exists. If not, use fallback.
+    // 4. Check whether the command exists. If not, use fallback.
     if (!property_exists($command_map, $command)) {
       $command = $fallback_command;
       $query = $raw_query;
       $fallback_used = true;
     }
 
-    // 4. Retrieve the command's executeQuery function
+    // 5. Retrieve the command's executeQuery function
     $command_controller_filepath = $command_map->{$command};
+    require_once(__DIR__ . '/../__definitions__/ICommandController.php');
+    require_once(__DIR__ . '/../__definitions__/Result.php');
     require_once($command_controller_filepath);
     $command_controller_name = basename($command_controller_filepath, '.command.php');
     $command_executor = [$command_controller_name, 'executeQuery'];
 
-    // 5. Check for debug mode
+    // 6. Check for debug mode
     $debug = isset($_GET['debug']) ? ($_GET['debug'] == '1') : false;
 
     return [
@@ -67,6 +87,7 @@ class SearchController {
       'command' => $command,
       'command_executor' => $command_executor,
       'fallback_used' => $fallback_used,
+      'raw_query' => $raw_query,
       'query' => $query
     ];
   }
@@ -90,7 +111,7 @@ class SearchController {
   private static function endConnection() {
     header('Connection: close');
     header('Content-Length: ' . ob_get_length());
-    // TODO: Two out of these three are not required. :P
+    // TODO: Two out of these three are supposedly not required.
     ob_end_flush();
     ob_flush();
     flush();
